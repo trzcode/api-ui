6ce2e745df1214a7a5064873ea91ceedf18bef54
trzcode <github@tristan-zur.de>
2023-03-21 19:51:39 +0100

    adds urls instead of one url

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -4,6 +4,10 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json",
+    urls: [
+        { url: "swaggers/petstore.json", name: "Petstore"},
+        { url: "swaggers/thecodebuzz.json", name: "TheCodeBuzz"}
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
